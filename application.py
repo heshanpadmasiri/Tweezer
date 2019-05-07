cad6d0ee2ce2ce20706a08e5ac48c43c9286a9f4
thilakshiK <35171029+thilakshiK@users.noreply.github.com>
2019-05-07 10:42:51 +0530

    Add files via upload

---
+++ application.py
@@ -0,0 +1,112 @@
+from flask import Flask , render_template , url_for, request
+import pyodbc
+from flask_cors import CORS, cross_origin
+from streamnew import streaming
+from piechart import percentage
+from timeline import createTimeline
+from worldmap import createMap
+
+#creating the database connection
+class DB:
+    def __init__(self):
+        self.server = 'tcp:twitter-server.database.windows.net'
+        self.database = 'twitter_db' 
+        self.username = 'thilakshi' 
+        self.password = 'tikiz<3kav' 
+        self.cnxn = pyodbc.connect('Driver={ODBC Driver 13 for SQL Server};Server=tcp:twitter-server.database.windows.net,1433;Database=twitter_db;Uid=thilakshi@twitter-server;Pwd=tikiz<3kav;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+        self.cursor = self.cnxn.cursor()
+
+    def getConnection(self):
+        return self.cursor
+
+    def execute(self,query):
+        self.cursor.execute(query)
+        return self.cursor
+
+    def insert(self,query):
+        self.cursor.execute(query)
+        self.cnxn.commit()
+        return self.cursor
+
+db = DB()
+
+
+app = Flask(__name__)
+CORS(app)
+
+#home page
+@app.route('/',methods=['GET' , 'POST'])
+def home():
+    global db
+    
+    if request.method == 'POST':
+        
+        #fetch form data
+        keyword = request.form['keyword']
+
+        #delete existing data in tables before the new keyword is searched
+        
+        db.insert("delete from emotiontb")    
+        db.insert("delete from tweetstb")
+        db.insert("delete from count")
+        db.insert("delete from percentagetb")
+
+        
+        streaming(str(keyword))    #streaming the keyword from twitter
+        percentage()            #calculating emotion percentages
+        
+        
+    else:
+        #display nothing in tabs if no keyword is searched
+        db.insert("delete from emotiontb")    
+        db.insert("delete from tweetstb")
+        db.insert("delete from count")
+        db.insert("delete from percentagetb")
+
+        
+    return render_template('index.html')
+
+#map page
+@app.route("/map")
+def worldmap():
+    global db
+    coordinate_list = createMap()    # retrieve the list of tweets with coordinates 
+    return render_template("map.html", coordinate_list= coordinate_list)
+
+#timeline page
+@app.route("/timeline")
+def timeline():
+    global db
+
+    #retrieve lists containing emotion counts
+    anger, disgust , fear, joy , sadness , surprise = createTimeline()
+
+    
+    return render_template("timeline.html", anger=anger, disgust = disgust, fear = fear, joy = joy, sadness = sadness, surprise = surprise)
+   
+
+#tweets page
+@app.route("/tweets")
+def tweets():
+    global db
+
+    #retrieving details of tweets in order to represent in the table
+    resultValue = db.execute("select tweetstb.createdAt, tweetstb.tweet, emotiontb.emotion from tweetstb inner join emotiontb on (tweetstb.tweetID = emotiontb.tweetID)")
+   
+    return render_template("tweets.html", resultValue = resultValue)
+
+
+#pie chart page
+@app.route("/piechart")
+def piechart():
+    global db
+       
+    output = db.execute("Select * from percentagetb")   #retrieving emotion percentages from the database
+    percentage_list = [(r[0],round(r[1], 2)) for r in output]
+    
+    return render_template("piechart.html" , percentage_list = percentage_list )
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True, threaded=True)           #start the web server
---
+++ piechart.py
@@ -0,0 +1,100 @@
+import pyodbc
+
+
+class DB:
+    def __init__(self):
+        self.server = 'tcp:twitter-server.database.windows.net'
+        self.database = 'twitter_db' 
+        self.username = 'thilakshi' 
+        self.password = 'tikiz<3kav' 
+        self.cnxn = pyodbc.connect('Driver={ODBC Driver 13 for SQL Server};Server=tcp:twitter-server.database.windows.net,1433;Database=twitter_db;Uid=thilakshi@twitter-server;Pwd=tikiz<3kav;Encrypt=yes;TrustServerCertificate=no;Connection Timeout=30;')
+        self.cursor = self.cnxn.cursor()
+
+    def getConnection(self):
+        return self.cursor
+
+    def execute(self,query):
+        self.cursor.execute(query)
+        return self.cursor
+
+    def insert(self,query):
+        self.cursor.execute(query)
+        self.cnxn.commit()
+        return self.cursor
+    
+#Create the DB connection 
+db = DB()
+
+#calulating percentages for each emotion
+
+def percentage():
+    global db
+    
+    output = db.execute("Select emotion from emotiontb")
+    emotionList = []
+
+    for e in output:
+        emotionList.append(e[0])
+
+        
+    list_len = len(emotionList)
+    anger_count = 0
+    disgust_count = 0
+    fear_count = 0
+    joy_count = 0
+    sadness_count = 0
+    surprise_count = 0
+
+    #calculating tweet count for each emotion
+    
+    for e in emotionList:
+        if (e == "Anger"):
+            anger_count+=1
+            
+        elif(e == "Disgust"):
+             disgust_count+=1
+
+        elif(e == "Fear"):
+             fear_count+=1
+        
+        elif(e == "Joy"):
+             joy_count+=1
+    
+        elif(e == "Sadness"):
+             sadness_count+=1
+
+        else:
+             surprise_count+=1
+
+    try:
+        #calculating the percentage of tweet count for each emotion
+        
+        anger_perc = round((anger_count/list_len)*100 , 2)
+        disgust_perc = round((disgust_count/list_len)*100 , 2)
+        fear_perc = round((fear_count/list_len)*100, 2)
+        joy_perc = round((joy_count/list_len)*100, 2)
+        sadness_perc = round((sadness_count/list_len)*100, 2)
+        surprise_perc = round((surprise_count/list_len)*100 ,2)
+
+    except (ZeroDivisionError):
+        pass
+
+    else:
+
+        emotion_dict = {"Anger" : anger_perc , "Disgust" : disgust_perc ,
+                    "Fear" : fear_perc , "Joy" : joy_perc, "Sadness" : sadness_perc , "Surprise" : surprise_perc}      
+
+
+        #inserting percentages of each emotion to the database
+        
+        for key in emotion_dict :
+            query = "insert into percentagetb (emotion , percen) values ('{0}','{1}')".format(key, emotion_dict[key])
+            db.insert(query)
+
+      
+    
+
+
+
+
+
